c9f23728ac37cf85cdeb5310dd7c88a46010b29d
paulzfm <zhufengminpaul@163.com>
2019-09-20 00:58:52 +0800

    doc ASM printer

--- src/main/java/decaf/lowlevel/AsmCodePrinter.java
+++ src/main/java/decaf/lowlevel/AsmCodePrinter.java
@@ -13,82 +13,113 @@ public class AsmCodePrinter {
 
     protected final String END_LINE = "\n";
 
+    protected final String COMMENT_PROMPT = "#";
+
+    /**
+     * Format print, with indents.
+     *
+     * @param fmt  format
+     * @param args arguments
+     */
     public void print(String fmt, Object... args) {
         sb.append(INDENTS);
         sb.append(String.format(fmt, args));
     }
 
+    /**
+     * Format print a line, with indents.
+     *
+     * @param fmt  format
+     * @param args arguments
+     */
     public void println(String fmt, Object... args) {
         sb.append(INDENTS);
         sb.append(String.format(fmt, args));
         sb.append(END_LINE);
     }
 
+    /**
+     * Simply print a newline.
+     */
     public void println() {
         sb.append(END_LINE);
     }
 
-    public void printLabel(Label lbl) {
-        // for debug
-        sb.append(".globl " + lbl.name);
-        sb.append(END_LINE);
-
-        sb.append(lbl.name);
+    /**
+     * Print a label.
+     *
+     * @param label label
+     */
+    public void printLabel(Label label) {
+        sb.append(label.name);
         sb.append(":");
         sb.append(END_LINE);
     }
 
-    public void printLabel(Label lbl, String comment) {
-        // for debug
-        sb.append(".globl " + lbl.name);
-        sb.append(END_LINE);
-
-        sb.append(lbl.name);
+    /**
+     * Print a label with comment.
+     *
+     * @param label   label
+     * @param comment comment
+     */
+    public void printLabel(Label label, String comment) {
+        sb.append(label.name);
         sb.append(":");
-        sb.append("    # ");
-        sb.append(comment);
-        sb.append(END_LINE);
+        sb.append("  ").append(COMMENT_PROMPT).append(' ').append(comment).append(END_LINE);
     }
 
-    // TODO: very ugly
-
+    /**
+     * Print an instruction.
+     *
+     * @param instr instruction
+     */
     public void printInstr(NativeInstr instr) {
         if (instr.isLabel()) {
-            // for debug
-            sb.append(".globl " + instr.label.name);
-            sb.append(END_LINE);
-
             sb.append(instr.label);
             sb.append(":");
-            sb.append(END_LINE);
         } else {
             sb.append(INDENTS);
-            sb.append(instr.toString());
-            sb.append(END_LINE);
+            sb.append(instr);
         }
+
+        sb.append(END_LINE);
     }
 
+    /**
+     * Print an instruction, with comment.
+     *
+     * @param instr   instruction
+     * @param comment comment
+     */
     public void printInstr(NativeInstr instr, String comment) {
         if (instr.isLabel()) {
             sb.append(instr.label);
             sb.append(":");
         } else {
             sb.append(INDENTS);
-            sb.append(instr.toString());
+            sb.append(instr);
         }
 
-        sb.append("    # ");
-        sb.append(comment);
-        sb.append(END_LINE);
+        sb.append("  ").append(COMMENT_PROMPT).append(' ').append(comment).append(END_LINE);
     }
 
+    /**
+     * Print a comment.
+     *
+     * @param comment comment
+     */
     public void printComment(String comment) {
         sb.append(INDENTS);
-        sb.append("# ");
+        sb.append(COMMENT_PROMPT).append(' ');
         sb.append(comment);
         sb.append(END_LINE);
     }
 
+    /**
+     * Finish printing. Get the plain text of the assembly code.
+     *
+     * @return assembly code
+     */
     public String close() {
         return sb.toString();
     }
